ccee516ae1421e3bf89d0b6c0704a1d78cee6f26
a3onny <14324389+Assone@users.noreply.github.com>
2026-01-26 12:37:27 +0800

    chore: update pnpm workspace configuration to include package overrides and additional dependencies

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,11 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
-  server: {
-    port: 3001,
-  },
+	plugins: [tailwindcss(), tanstackStart(), viteReact()],
+	resolve: {
+		alias: [{ find: /^@\/(.*)/, replacement: "/src/$1" }],
+	},
+
+	server: {
+		port: 3001,
+	},
+	experimental: {
+		enableNativePlugin: true,
+	},
 });
