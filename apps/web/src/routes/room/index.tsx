1bb6cb9d33fec6d8be7f13494508a2e4be285e16
a3onny <14324389+Assone@users.noreply.github.com>
2026-01-26 14:55:37 +0800

    feat: implement JoinRoom component and integrate with room route

--- apps/web/src/routes/room/index.tsx
+++ apps/web/src/routes/room/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
+import { JoinRoom } from "@/components/join-room";
+import { getRoomSession } from "@/functions/room";
 
 const searchSchema = z.object({
 	roomName: z.string().optional(),
@@ -8,8 +10,20 @@ const searchSchema = z.object({
 export const Route = createFileRoute("/room/")({
 	component: RouteComponent,
 	validateSearch: searchSchema,
+	loader: async () => {
+		const roomSession = await getRoomSession();
+
+		return roomSession;
+	},
 });
 
 function RouteComponent() {
-	return <div>Hello "/room/"!</div>;
+	const { roomName } = Route.useSearch();
+	const { displayName } = Route.useLoaderData();
+
+	return (
+		<div>
+			<JoinRoom displayName={displayName} roomName={roomName} />
+		</div>
+	);
 }
