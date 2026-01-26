1f782d302c12554ef8673bce388fc96b471668a9
a3onny <14324389+Assone@users.noreply.github.com>
2026-01-26 14:58:45 +0800

    feat: enhance room route with session management and redirection logic

--- apps/web/src/routes/room/$roomName.tsx
+++ apps/web/src/routes/room/$roomName.tsx
@@ -1,9 +1,33 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getRoomSession } from "@/functions/room";
 
-export const Route = createFileRoute('/room/$roomName')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/room/$roomName")({
+	component: RouteComponent,
+	beforeLoad: async () => {
+		const roomSession = await getRoomSession();
+
+		return roomSession;
+	},
+	loader: ({ context, params }) => {
+		const { clientId, displayName } = context;
+
+		if (!(clientId && displayName)) {
+			throw redirect({
+				to: "/room",
+				search: {
+					roomName: params.roomName,
+				},
+				replace: true,
+			});
+		}
+
+		return {
+			clientId,
+			displayName,
+		};
+	},
+});
 
 function RouteComponent() {
-  return <div>Hello "/room/$roomName"!</div>
+	return <div>Hello "/room/$roomName"!</div>;
 }
