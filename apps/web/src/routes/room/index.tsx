b3c111407b45f60d5c8264c6f50397c25f7a336e
a3onny <14324389+Assone@users.noreply.github.com>
2026-01-26 13:21:01 +0800

    feat: add search validation schema for room route

--- apps/web/src/routes/room/index.tsx
+++ apps/web/src/routes/room/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
-export const Route = createFileRoute('/room/')({
-  component: RouteComponent,
-})
+const searchSchema = z.object({
+	roomName: z.string().optional(),
+});
+
+export const Route = createFileRoute("/room/")({
+	component: RouteComponent,
+	validateSearch: searchSchema,
+});
 
 function RouteComponent() {
-  return <div>Hello "/room/"!</div>
+	return <div>Hello "/room/"!</div>;
 }
